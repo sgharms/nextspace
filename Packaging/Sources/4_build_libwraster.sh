2f51cbc320026b4cf82907b0b6682b6476113e8b
Steven G. Harms <sgharms@stevengharms.com>
2025-08-22 17:12:42 -0400

    4_build_libwraster.sh: ${ECHO} -> ECHO

--- Packaging/Sources/4_build_libwraster.sh
+++ Packaging/Sources/4_build_libwraster.sh
@@ -6,12 +6,12 @@
 #----------------------------------------
 # Install package dependecies
 #----------------------------------------
-${ECHO} ">>> Installing ${OS_ID} packages for WRaster library build"
+ECHO ">>> Installing ${OS_ID} packages for WRaster library build"
 if [ ${OS_ID} = "debian" ] || [ ${OS_ID} = "ubuntu" ]; then
-	${ECHO} "Debian-based Linux distribution: calling 'apt-get install'."
+	ECHO "Debian-based Linux distribution: calling 'apt-get install'."
 	sudo apt-get install -y ${WRASTER_DEPS} || exit 1
 else
-	${ECHO} "RedHat-based Linux distribution: calling 'yum -y install'."
+	ECHO "RedHat-based Linux distribution: calling 'yum -y install'."
 	SPEC_FILE=${PROJECT_DIR}/Libraries/libwraster/libwraster.spec
 	DEPS=`rpmspec -q --buildrequires ${SPEC_FILE} | grep -v nextspace-core-devel | awk -c '{print $1}'`
 	sudo yum -y install ${DEPS} || exit 1
