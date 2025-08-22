0f81442594f29faac25f78a007a0223302780fa9
Steven G. Harms <sgharms@stevengharms.com>
2025-08-22 17:12:42 -0400

    2_build_libobjc2.sh: ${ECHO} -> ECHO

--- Packaging/Sources/2_build_libobjc2.sh
+++ Packaging/Sources/2_build_libobjc2.sh
@@ -6,8 +6,8 @@
 # Install package dependecies
 #----------------------------------------
 if [ ${OS_ID} != "debian" ] && [ ${OS_ID} != "ubuntu" ]; then
-	${ECHO} ">>> Installing ${OS_ID} packages for ObjC 2.0 runtime build"
-	${ECHO} "RedHat-based Linux distribution: calling 'yum -y install'."
+	ECHO ">>> Installing ${OS_ID} packages for ObjC 2.0 runtime build"
+	ECHO "RedHat-based Linux distribution: calling 'yum -y install'."
 	SPEC_FILE=${PROJECT_DIR}/Libraries/libobjc2/libobjc2.spec
 	DEPS=`rpmspec -q --buildrequires ${SPEC_FILE} | grep -v "libdispatch-devel" | awk -c '{print $1}'`
 	sudo yum -y install ${DEPS} || exit 1
