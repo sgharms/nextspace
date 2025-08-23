28116ab353ecf8cae483013591391fbf45cd9eb6
Steven G. Harms <sgharms@stevengharms.com>
2025-08-23 04:32:06 -0400

    3_build_core.sh: Commented code cleanup

--- Packaging/Sources/3_build_core.sh
+++ Packaging/Sources/3_build_core.sh
@@ -35,10 +35,6 @@ $CP_CMD -v ${CORE_SOURCES}/etc/ld.so.conf.d/nextspace.conf $DEST_DIR/etc/ld.so.c
 $PRIV_CMD ldconfig
 
 # X11
-#if ! [ -d $DEST_DIR/etc/X11/xorg.conf.d ];then
-#	$MKDIR_CMD -v $DEST_DIR/etc/X11/xorg.conf.d
-#fi
-#$CP_CMD ${CORE_SOURCES}/etc/X11/xorg.conf.d/*.conf $DEST_DIR/etc/X11/xorg.conf.d/
 $CP_CMD ${CORE_SOURCES}/etc/X11/Xresources.nextspace $DEST_DIR/etc/X11
 
 # PolKit & udev
