b5b865e5e3ff6204de47d04acc8b7c3cc2a7952a
Steven G. Harms <sgharms@stevengharms.com>
2025-09-11 23:41:06 -0400

    Terminal: main.m: Split up files
    Figuring out how the app launches was confusing.
    
    We have a GNUmakefile that specifies:
    
    Terminal_MAIN_MODEL_FILE = Terminal.gorm
    
    `$LANG.lproj/Terminal.gorm` exists
    
    It, in turn, defines the "File's Owner" as
    `TerminalApplication`. But, before this commit,
    that didn't exist.
    
    Grepping might yield that this class lives in
    `main.m`! That's unusual. There's no really
    compelling reason to do this. Additionally there
    are some strange forward directions (e.g.
    `NSWindow.h`) that make collaborators wonder about
    their understanding of Objective-C.
    
    This change gets main.m to be a shell so that
    surprises and "Huh" are reduced.

---
+++ Applications/Terminal/TerminalApplication.h
@@ -0,0 +1,2 @@
+@interface TerminalApplication : NSApplication
+@end
---
+++ Applications/Terminal/TerminalApplication.m
@@ -0,0 +1,103 @@
+/*
+  Copyright (c) 2002, 2003 Alexander Malmberg <alexander@malmberg.org>
+  Copyright (c) 2015-2017 Sergii Stoian <stoyan255@gmail.com>
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; version 2 of the License.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+#import <Foundation/NSDebug.h>
+
+#import <AppKit/NSApplication.h>
+#import <AppKit/NSWindow.h>
+#import <AppKit/NSEvent.h>
+
+#import "TerminalApplication.h"
+#import "Defaults.h"
+
+
+@implementation TerminalApplication
+
+- (void)sendEvent:(NSEvent *)e
+{
+  if ([e type] == NSKeyDown && [e modifierFlags] & NSCommandKeyMask &&
+			[[Defaults shared] alternateAsMeta])
+  {
+    NSDebugLLog(@"key", @"intercepting key equivalent");
+
+    if ([[Defaults shared] alternateAsMeta] &&
+        [[[NSUserDefaults standardUserDefaults] objectForKey:@"PreserveReadlineWordMovement"] boolValue])
+		{
+      NSDebugLLog(@"key", @"Steven Terminal.app ergonomics");
+
+      if ([[e characters] length] == 1)
+      {
+        unichar c = [[e characters] characterAtIndex:0];
+				NSDebugLLog(@"key", @"Saw %hu", c);
+
+        // Preserve readline-esque Meta+f and Meta+b for whole-word operations
+        if ((c == 'f' || c == 'b') && !([e modifierFlags] & NSShiftKeyMask))
+        {
+          NSString *metaSeq = [NSString stringWithFormat:@"\033%c", c];
+
+          NSEvent *escEvent =
+            [NSEvent keyEventWithType:NSKeyDown
+                             location:[e locationInWindow]
+                        modifierFlags:0
+                            timestamp:[e timestamp]
+                         windowNumber:[e windowNumber]
+                              context:[e context]
+                           characters:metaSeq
+          charactersIgnoringModifiers:metaSeq
+                            isARepeat:[e isARepeat]
+                              keyCode:[e keyCode]];
+
+          [super sendEvent:escEvent];
+          return; // swallow original Alt+f / Alt+b
+        }
+      }
+			else // char length > 1
+			{
+				[[e window] sendEvent:e];
+			}
+		} // wasn't alt-as-meta; no PreserveReadlineWordMovement default
+  } // was a keyDown menu equivalent
+
+  if ([e type] == NSKeyDown && 
+			[[Defaults shared] alternateAsMeta] &&
+      ([e keyCode] == (NSNumber *)[[Defaults shared] superKeyKeycode]) &&
+      [[Defaults shared] swallowSuperKey] &&
+      [[e characters] length] == 1)
+  {
+    /* Swallow the super key (used to Super + Tab to change applications)
+     *
+     * This works because we don't pass the event down into the [e window], but
+     * rather let the event bubble /up/ to the desktop layer
+     *
+     */
+    NSLog(@"saw super key KEYDOWN");
+
+    NSDebugLLog(@"key", @"AAA: got key flags=%08lx  repeat=%i '%@' '%@' %4i %04x %lu %04x %lu\n",
+              [e modifierFlags], [e isARepeat], [e characters], [e charactersIgnoringModifiers],
+              [e keyCode], [[e characters] characterAtIndex:0], [[e characters] length],
+              [[e charactersIgnoringModifiers] characterAtIndex:0],
+              [[e charactersIgnoringModifiers] length]);
+    NSDebugLLog(@"key", @"Done");
+    return;
+  }
+
+  [super sendEvent:e];
+}
+
+@end
+
